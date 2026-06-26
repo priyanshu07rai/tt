a9f2279c671639139547ae9a84e3fc9299b28b18
Harshit Harsh <harshitharsh659@gmail.com>
2026-06-26 14:28:48 +0530

    Update axiosInstance.js

--- frontend-react/src/components/axiosInstance.js
+++ frontend-react/src/components/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
 const axiosInstance = axios.create({
 
     baseURL:import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api/"
