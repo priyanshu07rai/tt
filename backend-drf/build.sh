37287fd0e1f212acab3a75bd81894c04de5070f5
raipr <q2494301@gmail.com>
2026-06-27 13:36:18 +0530

    Auto-create superuser on build

--- backend-drf/build.sh
+++ backend-drf/build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('raipr', 'admin@example.com', '123321') if not User.objects.filter(username='raipr').exists() else None"
